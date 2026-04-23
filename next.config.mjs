9344e94164d666ff882f14271a7ba5b5d3d703f9
Annathewitch <108777580+Annathewitch@users.noreply.github.com>
2026-04-23 22:45:44 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['api.mapbox.com', 'images.unsplash.com'],
-  },
+  // 暂时不要加任何复杂的重定向或特殊的路径配置
 };
 export default nextConfig;
